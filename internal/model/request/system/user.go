da635d795b593248b0a4db8ff262580ecba99785
zhangxinglei <zhanghanglei@ienglish.cn>
2024-09-01 16:36:13 +0800

    update: 用户名唯一，显示昵称

--- internal/model/request/system/user.go
+++ internal/model/request/system/user.go
@@ -10,6 +10,7 @@ package system
 type CreateUserReq struct {
 	Username string `json:"userName" binding:"required"`            // 用户登录名
 	Password string `json:"password" aes:"true" binding:"required"` // 用户登录密码
+	NickName string `json:"nickName" binding:"required"`            // 用户昵称
 	Avatar   string `json:"avatar"`
 	Email    string `json:"email" binding:"required,email"`
 	Phone    string `json:"phone" binding:"required"`
--- internal/model/system/user.go
+++ internal/model/system/user.go
@@ -13,9 +13,9 @@ import (
 
 type User struct {
 	model.BaseModel
-	Username string `json:"userName" gorm:"index;comment:用户登录名"`       // 用户登录名
-	Password string `json:"-"  gorm:"comment:用户登录密码"`                  // 用户登录密码
-	NickName string `json:"nickName" gorm:"default:系统用户;comment:用户昵称"` // 用户昵称
+	Username string `json:"userName" gorm:"index;not null;unique;comment:用户登录名"` // 用户登录名
+	Password string `json:"-"  gorm:"comment:用户登录密码"`                            // 用户登录密码
+	NickName string `json:"nickName" gorm:"default:系统用户;comment:用户昵称"`           // 用户昵称
 	Avatar   string `gorm:"column:avatar;comment:'用户头像';type:longtext" json:"avatar"`
 	Email    string `gorm:"column:email;comment:'邮箱';size:128" json:"email"`
 	Phone    string `gorm:"column:phone;comment:'手机号码';size:11" json:"phone"`
